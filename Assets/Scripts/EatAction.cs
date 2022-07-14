8e9285753d87317d096cdc422cd4a2d31c5bdcfc
Miles Debinski <miles.debinski@gmail.com>
2022-07-14 13:31:19 +0100

    fix rotation towards food

--- Assets/Scripts/EatAction.cs
+++ Assets/Scripts/EatAction.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class EatAction : MonoBehaviour
 {
-    Movement _movement;
-    int foodEaten = 0;
-    void Start()
-    {
-        _movement = GetComponent<Movement>();
-    }
+  Movement _movement;
+  int foodEaten = 0;
+  void Start()
+  {
+    _movement = GetComponent<Movement>();
+  }
 
-    void Update()
-    {
-        
-    }
+  void Update()
+  {
 
-    private void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.tag == SharedScripts.GlobalTags.FOOD_TAG) {
-            _movement._Target = null;
-            foodEaten++;
-            Destroy(other.gameObject);
-        }
+  }
+
+  private void OnCollisionEnter2D(Collision2D other)
+  {
+    if (other.gameObject.tag == SharedScripts.GlobalTags.FOOD_TAG)
+    {
+      _movement.Target = null;
+      foodEaten++;
+      Destroy(other.gameObject);
     }
+  }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,65 +4,83 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    const int NEGATIVE_ROTATION = 1;
-    const int POSITIVE_ROTATION = -1;
+  const int NEGATIVE_ROTATION = 1;
+  const int POSITIVE_ROTATION = -1;
 
-    [SerializeField] float Speed = 10;
-    Rigidbody2D _npcBody {get; set;}
-    float _rotation = 0;
-    int _rotationType = POSITIVE_ROTATION;
-    public GameObject _Target = null;
-    
-    
-    void Start()
+  [SerializeField] float movementSpeed = 5;
+  [SerializeField] float rushSpeed = 10; //Miles* This is for rushing towards food if detected
+  [SerializeField] float rotationSpeed; //Miles* This is used for smooth rotation
+  [SerializeField] GameObject _target = null;
+
+  Rigidbody2D _npcBody { get; set; }
+  int _rotationType = POSITIVE_ROTATION;
+
+  private float _randomRotationAngle;
+  private Quaternion _rotation; //Miles* Use Quaternion instead of float to keep rortation data
+
+
+  #region get & set
+  public GameObject Target { set => _target = value; }
+  #endregion
+
+
+
+  void Start()
+  {
+    _npcBody = GetComponent<Rigidbody2D>();
+  }
+
+  void Update()
+  {
+    MoveForward(); //Miles* Move forward all the time, regardless if you see food or not. 
+  }
+
+  void MoveForward()
+  {
+    _npcBody.velocity = !_target ? transform.up * movementSpeed : transform.up * rushSpeed; //Miles* if targeting food, speed up towards it! :D
+    transform.rotation = _rotation;
+  }
+
+  void RotateOnTarget()
+  {
+    Vector3 direction = _target.transform.position - transform.position;
+    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; //Miles* Angle between x and y 
+    Quaternion q = Quaternion.AngleAxis(angle - 90, Vector3.forward); //Miles* - 90 degree to face the target directly
+    _rotation = q;
+
+    ////Miles* ==== USE LINE BELOW IF YOU WANT SMOOTH ROTATION, YOU WILL NEED TO RUN IT ON UPDATE TO MAKE IT WORK ====
+    // transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * rotationSpeed); //Miles* Average out 2 values, higher rotationSpeed = faster rotation.
+  }
+
+  void RotateOnWall()
+  {
+    _randomRotationAngle += _rotationType * Random.Range(5, 30);
+    _rotation = Quaternion.Euler(0, 0, _randomRotationAngle);
+  }
+
+  private void OnCollisionEnter2D(Collision2D other)
+  {
+    if (other.gameObject.tag == SharedScripts.GlobalTags.WALL_TAG)
     {
-        _npcBody = GetComponent<Rigidbody2D>();
+      var tmpRandom = Random.Range(0, 2);
+      _rotationType = tmpRandom == 0 ? POSITIVE_ROTATION : NEGATIVE_ROTATION;
     }
+  }
 
-    void Update()
+  private void OnCollisionStay2D(Collision2D other)
+  {
+    if (other.gameObject.tag == SharedScripts.GlobalTags.WALL_TAG)
     {
-        Go();
+      RotateOnWall();
     }
+  }
 
-    void Go() {
-        // 
-        if(!_Target){
-            _npcBody.velocity = transform.up * Speed;
-            transform.rotation = Quaternion.Euler(0, 0, _rotation);
-        }
-        else {
-            Vector3 dir = _Target.transform.position - transform.position;
-            Quaternion lookRotation = Quaternion.LookRotation(dir);
-            Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * 20f).eulerAngles;
-            transform.rotation = Quaternion.Euler(0f, 0f, rotation.z);
-        }
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.gameObject.tag == SharedScripts.GlobalTags.FOOD_TAG && !_target) //Miles* target next food only if target is null
+    {
+      _target = other.gameObject;
+      RotateOnTarget();
     }
-    void RotateOnWall() {
-        _rotation+= _rotationType * Random.Range(5, 30);
-    }
-    void RotateOnTarget() {
-        
-        // var direction = _Target.transform.position - transform.position;
-        // var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        // var q = Quaternion.AngleAxis(angle, Vector3.forward);
-        // transform.rotation = Quaternion.FromToRotation(transform.position, _Target.transform.position);
-    }
-    private void OnCollisionEnter2D(Collision2D other) {
-         if(other.gameObject.tag == SharedScripts.GlobalTags.WALL_TAG) {
-            var tmpRandom = Random.Range(0,2);
-            _rotationType = tmpRandom == 0 ? POSITIVE_ROTATION : NEGATIVE_ROTATION; 
-        }
-    }
-    private void OnCollisionStay2D(Collision2D other) {
-        if(other.gameObject.tag == SharedScripts.GlobalTags.WALL_TAG) {
-            RotateOnWall();
-        }
-    }
-    private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log(other.gameObject.tag);
-        if(other.gameObject.tag == SharedScripts.GlobalTags.FOOD_TAG) {
-            _Target = other.gameObject;
-            RotateOnTarget();
-        }
-    }
+  }
 }
